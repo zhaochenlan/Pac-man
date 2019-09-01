42233ac8cbb8166373a253bc8caab2a79a88df89
zhaochenlan <zhaochenlan@gmail.com>
2019-09-01 20:37:30 +1000

    more wayPoints

---
+++ Assets/Scrept/Map items/PowerUp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D co2)
+    {
+        if (co2.name == "pacman")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scrept/Monster AI/monster_movement_red.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monster_movement_red : monster_movement
+{
+    bool naviOn = false;
+
+    void Update()
+    {
+        
+    }
+
+    override protected void setUp()
+    {
+        animatorController.SetTrigger("down");
+        dest = transform.position;
+        weekUpTime = 5;
+    }
+
+    override protected wayPoint findNextWp()
+    {
+        if (naviOn == false)
+        {
+            if (GameObject.Find("pacman"))//如果发现吃豆人就追击
+            {
+                return naviTo(chase(GameObject.Find("pacman")));
+            }
+            //否则随机在当前路径点相邻点选择一个前进
+            int r;
+            do
+            {
+                r = Random.Range(0, nextWp.neighborWps.Length);
+            } while (nextWp.neighborWps[r] == lastWp);//防止返回上一个路径点
+
+            return nextWp.neighborWps[r];
+        }
+        else
+        {
+            return naviTo(this.naviWp);
+        }
+
+    }
+
+    private wayPoint chase(GameObject aimObj) {
+        wayPoint aimWp = null;
+        List<wayPoint> Wps = getWayPoints();
+        float minDistance = 999;
+        //选择与吃豆人距离最短的一个路径点并导航
+        for (int i = 0; i < Wps.Count; i++)
+        {
+            if (Vector3.Distance(aimObj.transform.position, Wps[i].transform.position) < minDistance)
+            {
+                aimWp = Wps[i];
+                minDistance = Vector3.Distance(aimObj.transform.position, Wps[i].transform.position);
+            }
+        }
+        return aimWp;
+    }
+
+}
